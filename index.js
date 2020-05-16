29cf702b7fe6ea22378c544ab4a81bd1ed645962
SreemanTummala <44180141+SreemanTummala@users.noreply.github.com>
2020-05-15 23:32:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+client.on("guildMemberAdd", (member) => {
+  const defaultChannel =member.guild.channels.cache.find(channel => channel.name==="general");
+  var i=0;
+  
+    defaultChannel.send("hi "+member.user.username+ " welcome to the UTDCS discord server");
+    return;  
+  
+});
+client.on("guildMemberRemove", (member) => {
+  const defaultChannel =member.guild.channels.cache.find(channel => channel.name==="general");
+  defaultChannel.send("bye "+member.user.username+ " good luck at utd(if you are going there ig)");
+    return;  
+});
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  console.log(message.id);
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+  if(message.content==='hola')
+  {
+  message.channel.send("hi");
+  return;
+  }
+});
+client.login('NzEwNjg0NTY4Njc5NzQzNTA4.Xr4EGg.5-lXuT2lQYhJk5C1fa5jeQk91og');
